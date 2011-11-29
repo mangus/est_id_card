f99fe5bc06fa073aa82b075a33dccd31ac91ca84
Mart Mangus <mart@mangus.ee>
2011-11-30 01:11:16 +0300

    Comments and little cleanup

--- auth.php
+++ auth.php
@@ -5,6 +5,8 @@
  *
  * Authentication Plugin: Login with Estonian ID-card
  *
+ * Real authentication takes place in file login.php (that file needs to have access to ID-card data)
+ *
  */
 
 require_once($CFG->dirroot.'/lib/authlib.php');
@@ -15,22 +17,27 @@ class auth_plugin_est_id_card extends auth_plugin_base {
     function auth_plugin_est_id_card() {
         $this->authtype = 'est_id_card';
     }
-    private function id_card_inserted()
-    {   // TODO
-        return false;
-    }
-    private function get_id_number_from_id_card()
-    {   // TODO
-        return '38401275724';
-    }
+
+    /** Login is going through file auth/est_id_card/login.php instead of usual login form */
     function user_login($username, $password) {
         return false;
     }
 
+    private function id_card_inserted()
+    {   // TODO
+        return false;
+    }
+
+    private function get_id_number()
+    {   // TODO
+        return '38401275724';
+    }
+
+    /** Real authentication here */
     function authenticate_with_id_card() {
         global $DB, $CFG, $SESSION;
         if ($this->id_card_inserted()) {
-            $conditions = array('idnumber' => $this->get_id_number_from_id_card());
+            $conditions = array('idnumber' => $this->get_id_number());
             $usertologin = $DB->get_record('user', $conditions, $fields='*');
             if ($usertologin !== false) {
                 $USER = complete_user_login($usertologin);
@@ -43,32 +50,19 @@ class auth_plugin_est_id_card extends auth_plugin_base {
         redirect($goto);
     }
 
-    /**
-     * Returns a list of potential IdPs that this authentication plugin supports.
-     * This is used to provide links on the login page.
-     *
-     * @param string $wantsurl the relative url fragment the user wants to get to.  You can use this to compose a returnurl, for example
-     *
-     * @return array like:
-     *              array(
-     *                  array(
-     *                      'url' => 'http://someurl',
-     *                      'icon' => new pix_icon(...),
-     *                      'name' => get_string('somename', 'auth_yourplugin'),
-     *                 ),
-     *             )
-     */
+    /** Creates "login with ID-card" link to Moodle login page */
     function loginpage_idp_list($wantsurl) {
-        global $OUTPUT;
         return array(
             array(
-                'url' => new moodle_url('https://h1.moodle.e-ope.ee/auth/est_id_card/login.php'),
-                'icon' => new pix_icon('idkaart', 'Login with Estonian ID-card'), //'https://h1.moodle.e-ope.ee/auth/est_id_card/images/idkaart.gif',
+                'url' => new moodle_url($CFG->wwwroot . '/auth/est_id_card/login.php'),
+                'icon' => new pix_icon('idkaart', 'Login with Estonian ID-card'),
+                    // Need to copy this file (cp auth/est_id_card/images/idkaart.gif pix/)
                 'name' => get_string('login_with', 'auth_est_id_card')
             )
         );
     }
 
+    /** Shows nice error messages to user */
     function loginpage_hook() {
         global $errormsg;
         if (isset($_GET['no_id_card_data'])) {
@@ -76,14 +70,6 @@ class auth_plugin_est_id_card extends auth_plugin_base {
         } else if (isset($_GET['no_user_with_id'])) {
             $errormsg = get_string('no_user_with_id', 'auth_est_id_card');
         }
-        /*
-        global $frm;  // can be used to override submitted login form
-        global $user; // can be used to replace authenticate_user_login()
-        echo "uhuu?";
-        var_dump($frm);
-        var_dump($user);
-        die('loginpage_hook');
-        */
     }
 }
 
