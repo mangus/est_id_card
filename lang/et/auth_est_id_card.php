946e1618c26f6ea52308a3d26a5503779a25c1dd
Mart Mangus <mart@mangus.ee>
2012-01-19 15:23:50 +0200

    Estonian translaiton

---
+++ lang/et/auth_est_id_card.php
@@ -0,0 +1,14 @@
+<?php
+
+//$string['auth_est_id_carddescription'] = 'Login with';
+$string['pluginname'] = 'Eesti ID-kaadriga sisselogimine';
+$string['login_with'] = 'Eesti ID-kaadriga sisselogimine';
+
+// Errors
+$string['no_user_with_id'] = 'Selle ID-kaardiga ei ole ükski Moodle kasutaja seotud';
+$string['no_id_card_data'] = 'Andmete lugemine ID-kaardilt ebaõnnestus';
+
+// Other pages
+$string['login_with_id_card'] = 'Logi sisse ID-kaardiga';
+$string['password_change'] = 'Salasõna on võimalik muuta ka ID-kaardiga sisselogides. Selleks kasuta all olevat linki:';
+
